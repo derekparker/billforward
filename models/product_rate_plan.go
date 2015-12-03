5c5e27e8c1501fc61fe17d13dadbdfc3559f1e2a
Chance Zibolski <chance@coreos.com>
2015-12-03 12:43:25 -0800

    pointer to interface

--- models/product_rate_plan.go
+++ models/product_rate_plan.go
@@ -11,8 +11,7 @@ import (
 	"github.com/go-swagger/go-swagger/strfmt"
 )
 
-/*
-A rate plan describes a pricing system under which a subscription can be made to a product.
+/*A rate plan describes a pricing system under which a subscription can be made to a product.
 
 swagger:model ProductRatePlan
 */
@@ -186,70 +185,87 @@ func (m *ProductRatePlan) Validate(formats strfmt.Registry) error {
 	var res []error
 
 	if err := m.validateBillingEntity(formats); err != nil {
+		// prop
 		res = append(res, err)
 	}
 
 	if err := m.validateCreateZeroValuedInvoices(formats); err != nil {
+		// prop
 		res = append(res, err)
 	}
 
 	if err := m.validateCurrency(formats); err != nil {
+		// prop
 		res = append(res, err)
 	}
 
 	if err := m.validateDurationPeriod(formats); err != nil {
+		// prop
 		res = append(res, err)
 	}
 
 	if err := m.validateFailedPaymentBehaviour(formats); err != nil {
+		// prop
 		res = append(res, err)
 	}
 
 	if err := m.validateInvoiceIssueType(formats); err != nil {
+		// prop
 		res = append(res, err)
 	}
 
 	if err := m.validateIssuePeriod(formats); err != nil {
+		// prop
 		res = append(res, err)
 	}
 
 	if err := m.validateLocalisedTax(formats); err != nil {
+		// prop
 		res = append(res, err)
 	}
 
 	if err := m.validateMigrationBehaviour(formats); err != nil {
+		// prop
 		res = append(res, err)
 	}
 
 	if err := m.validateName(formats); err != nil {
+		// prop
 		res = append(res, err)
 	}
 
 	if err := m.validatePricingComponents(formats); err != nil {
+		// prop
 		res = append(res, err)
 	}
 
 	if err := m.validateProRataMode(formats); err != nil {
+		// prop
 		res = append(res, err)
 	}
 
 	if err := m.validateProductID(formats); err != nil {
+		// prop
 		res = append(res, err)
 	}
 
 	if err := m.validateProductType(formats); err != nil {
+		// prop
 		res = append(res, err)
 	}
 
 	if err := m.validateRecursionType(formats); err != nil {
+		// prop
 		res = append(res, err)
 	}
 
 	if err := m.validateTaxStatus(formats); err != nil {
+		// prop
 		res = append(res, err)
 	}
 
 	if err := m.validateTrialPeriod(formats); err != nil {
+		// prop
 		res = append(res, err)
 	}
 
@@ -271,7 +287,10 @@ func (m *ProductRatePlan) validateBillingEntityEnum(path, location string, value
 			productRatePlanBillingEntityEnum = append(productRatePlanBillingEntityEnum, v)
 		}
 	}
-	return validate.Enum(path, location, value, productRatePlanBillingEntityEnum)
+	if err := validate.Enum(path, location, value, productRatePlanBillingEntityEnum); err != nil {
+		return err
+	}
+	return nil
 }
 
 func (m *ProductRatePlan) validateBillingEntity(formats strfmt.Registry) error {
@@ -313,7 +332,10 @@ func (m *ProductRatePlan) validateDurationPeriodEnum(path, location string, valu
 			productRatePlanDurationPeriodEnum = append(productRatePlanDurationPeriodEnum, v)
 		}
 	}
-	return validate.Enum(path, location, value, productRatePlanDurationPeriodEnum)
+	if err := validate.Enum(path, location, value, productRatePlanDurationPeriodEnum); err != nil {
+		return err
+	}
+	return nil
 }
 
 func (m *ProductRatePlan) validateDurationPeriod(formats strfmt.Registry) error {
@@ -341,7 +363,10 @@ func (m *ProductRatePlan) validateFailedPaymentBehaviourEnum(path, location stri
 			productRatePlanFailedPaymentBehaviourEnum = append(productRatePlanFailedPaymentBehaviourEnum, v)
 		}
 	}
-	return validate.Enum(path, location, value, productRatePlanFailedPaymentBehaviourEnum)
+	if err := validate.Enum(path, location, value, productRatePlanFailedPaymentBehaviourEnum); err != nil {
+		return err
+	}
+	return nil
 }
 
 func (m *ProductRatePlan) validateFailedPaymentBehaviour(formats strfmt.Registry) error {
@@ -369,7 +394,10 @@ func (m *ProductRatePlan) validateInvoiceIssueTypeEnum(path, location string, va
 			productRatePlanInvoiceIssueTypeEnum = append(productRatePlanInvoiceIssueTypeEnum, v)
 		}
 	}
-	return validate.Enum(path, location, value, productRatePlanInvoiceIssueTypeEnum)
+	if err := validate.Enum(path, location, value, productRatePlanInvoiceIssueTypeEnum); err != nil {
+		return err
+	}
+	return nil
 }
 
 func (m *ProductRatePlan) validateInvoiceIssueType(formats strfmt.Registry) error {
@@ -393,7 +421,10 @@ func (m *ProductRatePlan) validateIssuePeriodEnum(path, location string, value s
 			productRatePlanIssuePeriodEnum = append(productRatePlanIssuePeriodEnum, v)
 		}
 	}
-	return validate.Enum(path, location, value, productRatePlanIssuePeriodEnum)
+	if err := validate.Enum(path, location, value, productRatePlanIssuePeriodEnum); err != nil {
+		return err
+	}
+	return nil
 }
 
 func (m *ProductRatePlan) validateIssuePeriod(formats strfmt.Registry) error {
@@ -426,7 +457,10 @@ func (m *ProductRatePlan) validateMigrationBehaviourEnum(path, location string, 
 			productRatePlanMigrationBehaviourEnum = append(productRatePlanMigrationBehaviourEnum, v)
 		}
 	}
-	return validate.Enum(path, location, value, productRatePlanMigrationBehaviourEnum)
+	if err := validate.Enum(path, location, value, productRatePlanMigrationBehaviourEnum); err != nil {
+		return err
+	}
+	return nil
 }
 
 func (m *ProductRatePlan) validateMigrationBehaviour(formats strfmt.Registry) error {
@@ -468,7 +502,10 @@ func (m *ProductRatePlan) validateProRataModeEnum(path, location string, value s
 			productRatePlanProRataModeEnum = append(productRatePlanProRataModeEnum, v)
 		}
 	}
-	return validate.Enum(path, location, value, productRatePlanProRataModeEnum)
+	if err := validate.Enum(path, location, value, productRatePlanProRataModeEnum); err != nil {
+		return err
+	}
+	return nil
 }
 
 func (m *ProductRatePlan) validateProRataMode(formats strfmt.Registry) error {
@@ -505,7 +542,10 @@ func (m *ProductRatePlan) validateProductTypeEnum(path, location string, value s
 			productRatePlanProductTypeEnum = append(productRatePlanProductTypeEnum, v)
 		}
 	}
-	return validate.Enum(path, location, value, productRatePlanProductTypeEnum)
+	if err := validate.Enum(path, location, value, productRatePlanProductTypeEnum); err != nil {
+		return err
+	}
+	return nil
 }
 
 func (m *ProductRatePlan) validateProductType(formats strfmt.Registry) error {
@@ -529,7 +569,10 @@ func (m *ProductRatePlan) validateRecursionTypeEnum(path, location string, value
 			productRatePlanRecursionTypeEnum = append(productRatePlanRecursionTypeEnum, v)
 		}
 	}
-	return validate.Enum(path, location, value, productRatePlanRecursionTypeEnum)
+	if err := validate.Enum(path, location, value, productRatePlanRecursionTypeEnum); err != nil {
+		return err
+	}
+	return nil
 }
 
 func (m *ProductRatePlan) validateRecursionType(formats strfmt.Registry) error {
@@ -553,7 +596,10 @@ func (m *ProductRatePlan) validateTaxStatusEnum(path, location string, value str
 			productRatePlanTaxStatusEnum = append(productRatePlanTaxStatusEnum, v)
 		}
 	}
-	return validate.Enum(path, location, value, productRatePlanTaxStatusEnum)
+	if err := validate.Enum(path, location, value, productRatePlanTaxStatusEnum); err != nil {
+		return err
+	}
+	return nil
 }
 
 func (m *ProductRatePlan) validateTaxStatus(formats strfmt.Registry) error {
@@ -581,7 +627,10 @@ func (m *ProductRatePlan) validateTrialPeriodEnum(path, location string, value s
 			productRatePlanTrialPeriodEnum = append(productRatePlanTrialPeriodEnum, v)
 		}
 	}
-	return validate.Enum(path, location, value, productRatePlanTrialPeriodEnum)
+	if err := validate.Enum(path, location, value, productRatePlanTrialPeriodEnum); err != nil {
+		return err
+	}
+	return nil
 }
 
 func (m *ProductRatePlan) validateTrialPeriod(formats strfmt.Registry) error {
